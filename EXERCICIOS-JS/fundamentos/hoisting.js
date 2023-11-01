1384981c1a672d07b398549f8593bb3069f66291
gustamba <gustamba7813@gmail.com>
2023-11-01 14:47:32 -0300

    Upload via code-server

--- EXERCICIOS-JS/fundamentos/hoisting.js
+++ EXERCICIOS-JS/fundamentos/hoisting.js
@@ -1,3 +1,7 @@
 console.log('a = ', a);
 var a = 2;
 console.log('a = ', a);
+
+//console.log('b = ', b) #Da erro
+let b = 2
+console.log('b = ', b)
---
+++ EXERCICIOS-JS/fundamentos/parNomeValor.js
@@ -0,0 +1,22 @@
+// par nome/valor
+const saudacao = 'Opa'; // contexto lexico 1
+
+function exec() {
+    const saudacao = 'Falaaa'; // contexto lexico 2
+    return saudacao;
+}
+
+// Objetos sao grupos aninhados de pares nome/valor
+const cliente = {
+    nome: 'Pedro', 
+    idade: 32, 
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua Muito legal', 
+        numero: 123
+    }
+}
+
+console.log(saudacao);
+console.log(exec());
+console.log(cliente)
