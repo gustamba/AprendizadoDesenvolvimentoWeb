8727e66bad8df6dcf143fff1e24a0e7c942ff31e
gustamba <gustamba7813@gmail.com>
2024-04-26 12:06:33 -0300

    pois e

---
+++ EXERCICIOS-JS/objeto/objetosConstantes.js
@@ -0,0 +1,16 @@
+// pessoa -> 123 -> {...}
+const pessoa = { nome: 'Joao'}
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+// pessoa -> 456 -> {...}
+// pessoa = { nome: 'Ana' }
+
+Object.freeze(pessoa)
+
+pessoa.nome = 'Maria'
+pessoa.end = 'Rua ABC'
+delete pessoa.nome
+
+console.log(pessoa.nome)
+console.log(pessoa)
--- EXERCICIOS-JS/projCal/script.js
+++ EXERCICIOS-JS/projCal/script.js
@@ -1,40 +1,50 @@
 let valores = []
 let numero = ''
-
+let limpar = false;
 
 function adicionarValor(valor) {
+    if(limpar){
+        document.getElementById("resultado").innerText = ""; 
+        limpar = false;
+    }
     numero += valor.toString()
+    console.log('Número digitado: ' + numero)
+    document.getElementById("resultado").innerText += valor.toString();
 }
 
 function adicionarOperacao(operacao) {
     valores.push(numero.toString())
     valores.push(operacao)
     numero = ''
+    console.log('Numero digitado: ' + valores[0])
+    console.log('Operação: ' + valores[1]);
+    document.getElementById("resultado").innerText += operacao.toString();
 }
 
 function resultado() {
+    valores.push(numero.toString())
     let solucao = 0
     switch(valores[1]) {
         case '+':
-            solucao = parseInt(valores[0]) + parseInt(valores[2])
+            solucao = parseInt(valores[0]) + parseInt(valores[2]);
             break
         case '-':
-            solucao = parseInt(valores[0]) - parseInt(valores[2])
+            solucao = parseInt(valores[0]) - parseInt(valores[2]);
             break
         case '*':
-            solucao = parseInt(valores[0]) * parseInt(valores[2])
+            solucao = parseInt(valores[0]) * parseInt(valores[2]);
             break
         case '/':
-            solucao = parseInt(valores[0]) / parseInt(valores[2])
+            solucao = parseInt(valores[0]) / parseInt(valores[2]);
             break
         default:
-            solucao = 'Error'
+            solucao = 'Selecione uma operação';
             break
     }
-    return solucao
+    console.log(valores, solucao);
+    let resultadoDisplay = document.getElementById("resultado");
+    resultadoDisplay.innerText = solucao;
+    numero = '';
+    valores = [];
+    limpar = true;
 }
-
-adicionarValor('1')
-adicionarOperacao('+')
-adicionarValor('2')
-console.log(resultado())
