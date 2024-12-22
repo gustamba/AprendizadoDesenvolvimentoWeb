6b89ad29ada41cbe6db37b5c728184d718bb8124
gustamba <gustamba7813@gmail.com>
2024-12-22 16:57:45 -0300

    Estudo API

--- EXERCICIOS-JS/node/projeto/src/servidor.js
+++ EXERCICIOS-JS/node/projeto/src/servidor.js
@@ -2,9 +2,39 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDado = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45 }) // Converter para JSON
+    res.send(bancoDeDado.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDado.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDado.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDado.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDado.excluirProdutos(req.params.id)
+    res.send(produto) // JSON
 })
 
 app.listen(porta, () => {
