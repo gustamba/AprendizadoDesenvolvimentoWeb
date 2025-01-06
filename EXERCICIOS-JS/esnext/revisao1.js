66692c95b5b079d84fef30d093f663966b55daab
gustamba <gustamba7813@gmail.com>
2025-01-06 17:30:53 -0300

    Revisão

---
+++ EXERCICIOS-JS/esnext/revisao1.js
@@ -0,0 +1,15 @@
+// let e const
+{
+    var a = 2
+    let b = 3
+    console.log(b)
+}
+console.log(a)
+
+// Template String
+const produto = 'iPad'
+console.log(`${produto} é caro!`)
+
+// Destructuring
+const [l, e, ...tras] = "Cod3r"
+console.log(l, e, tras)
