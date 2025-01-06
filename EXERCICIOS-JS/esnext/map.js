0ff254170a3af238a1e7cfebca7d8056f80045f2
Gustamba <gustamba7813@gmail.com>
2025-01-06 20:54:15 +0000

    recaptulando

---
+++ EXERCICIOS-JS/esnext/map.js
@@ -0,0 +1,21 @@
+const tecnologias = new Map()
+tecnologias.set('react', { framework: false })
+tecnologias.set('angular', { framework: true })
+
+console.log(tecnologias.react)
+console.log(tecnologias.get('react').framework)
+
+const chavesVariadas = new Map([
+    [function () {  }, 'Função'],
+    [{}, 'Objeto'],
+    [123, 'Número'],
+])
+
+chavesVariadas.forEach((vl, ch) => {
+    console.log(ch, vl)
+})
+
+console.log(chavesVariadas.has(123))
+chavesVariadas.delete(123)
+console.log(chavesVariadas.has(123))
+console.group(chavesVariadas.size)
