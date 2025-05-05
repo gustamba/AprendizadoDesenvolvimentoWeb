c524edeafac5097bf450a8d1d8d644c03e4c98b3
Gustamba <gustamba7813@gmail.com>
2025-05-05 20:20:34 -0300

    gulp sap estudos

--- EXERCICIOS-WEB/gulp/spa/gulpTasks/app.js
+++ EXERCICIOS-WEB/gulp/spa/gulpTasks/app.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify')
-const sass = require('gulp-sass')
+const sass = require('gulp-sass')(require('sass'));
 const uglifycss = require('gulp-uglifycss')
 const concat = require('gulp-concat')
 const htmlmin = require('gulp-htmlmin')
@@ -16,7 +16,7 @@ function appCSS() {
     return gulp.src('src/assets/sass/index.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(uglifycss({ "uglyComments": true }))
-        .pipe(conca('app.min.css'))
+        .pipe(concat('app.min.css'))
         .pipe(gulp.dest('build/assets/css'))
 }
 
